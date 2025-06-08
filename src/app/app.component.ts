62167ea0b964b0afe2349d5159aec6a7847f5345
Hem <hem@privatecircle.co>
2025-06-08 22:55:25 +0530

    Refactor Angular application structure and update component configurations
    - Changed the prefix in `angular.json` from `app` to `hr_point` for better project identification.
    - Removed unused `app.component.html`, `app.component.scss`, and `app.component.spec.ts` files to streamline the project.
    - Updated `app.component.ts` to use inline templates and styles for simplicity.
    - Adjusted border-radius in the popular dishes component for a refined UI.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,14 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `
+    <router-outlet />
+  `,
+  styles: `
+    :host {
+      display: block;
+    }
+  `
 })
 export class AppComponent {
   title = 'hr-foodpoint';
